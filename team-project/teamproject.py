06dedb28a52e55e194a3fe3fc36d19d0ed4e3e52
MinJeong277 <kk1061106@naver.com>
2021-05-09 17:11:35 +0900

    'teamproject4'

--- team-project/teamproject.py
+++ team-project/teamproject.py
@@ -1,9 +1,11 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url = 'https://coronaboard.kr'
+url = 'https://coronaboard.kr/'
 
 driver = webdriver.Chrome('C:/Users/82102/Desktop/first_git_project/team-project/chromedriver.exe')
 driver.get(url) # url 불러오기
@@ -23,5 +25,30 @@ print(df)
 
 driver.quit()
 
-df.iloc[0,2:4].plot(kind='bar')
+
+
+
+a=input()
+for i in range(75):
+    if a in df.iloc[i,1]:
+        nation=i
+        break
+list=[]
+c=''
+for i in range(2,5):
+    b=df.iloc[nation,i].split()[0]
+    c+=' '+b
+c=c.replace(',','').split()
+c = [int(i) for i in c]
+
+print(c)
+
+
+font_name = mpl.font_manager.FontProperties(fname='C:/Windows/Fonts/malgun.ttf').get_name()
+mpl.rc('font', family=font_name)
+x = np.arange(3)
+d=['확진자','사망자','완치']
+plt.bar(x, c)
+plt.xticks(x, d)
+plt.show()
 
